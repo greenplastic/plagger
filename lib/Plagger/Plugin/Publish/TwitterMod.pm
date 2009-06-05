f8e9569a008afe9d7a309e3e21c4b0d9ef8d03d8
yosshi <yosshi@gmail.com>
2009-06-06 02:23:31 +0900

    first commit

---
+++ lib/Plagger/Plugin/Publish/TwitterMod.pm
@@ -0,0 +1,69 @@
+package Plagger::Plugin::Publish::TwitterMod;
+use strict;
+use base qw( Plagger::Plugin );
+
+use Encode;
+use Net::Twitter;
+use Time::HiRes qw(sleep);
+use WWW::Shorten 'RevCanonical';
+use WWW::Shorten::Simple;
+
+sub register {
+    my($self, $context) = @_;
+    $context->register_hook(
+        $self,
+        'publish.entry' => \&publish_entry,
+        'plugin.init'   => \&initialize,
+    );
+}
+
+sub initialize {
+    my($self, $context) = @_;
+    my %opt = (
+        username => $self->conf->{username},
+        password => $self->conf->{password},
+    );
+    for my $key (qw/ apihost apiurl apirealm/) {
+        $opt{$key} = $self->conf->{$key} if $self->conf->{$key};
+    }
+    $self->{twitter} = Net::Twitter->new(%opt);
+}
+
+sub publish_entry {
+    my($self, $context, $args) = @_;
+
+    # add prefix to feed title
+    my $sitename = "";
+    if ( $args->{entry}->permalink =~ /flickr/ ) {$sitename = "[moblog] "};
+    if ( $args->{entry}->permalink =~ /crossreview/ ) {$sitename = "[crossreview] "};
+
+    # try RevCanonical then Bitly
+    my $bitly_username = $self->conf->{bitly_username};
+    my $bitly_api_key  = $self->conf->{bitly_api_key};
+
+    my @shorteners = (
+      WWW::Shorten::Simple->new('RevCanonical'), # Try this first
+      WWW::Shorten::Simple->new('Bitly', $bitly_username, $bitly_api_key), # Then fallback to Bitly
+    );
+
+    my $short_url;
+    for my $shortener (@shorteners) {
+      $short_url = eval { $shortener->shorten($args->{entry}->permalink) }
+	and last;
+    }
+
+    my $body = $sitename .  $args->{entry}->title  . " " . $short_url;
+
+    # TODO: FIX when Summary configurable.
+    if ( length($body) > 159 ) {
+        $body = substr($body, 0, 159);
+    }
+    $context->log(info => "Updating Twitter status to '$body'");
+    $self->{twitter}->update( encode_utf8($body) ) or $context->error("Can't update twitter status");
+
+    my $sleeping_time = $self->conf->{interval} || 15;
+    $context->log(info => "sleep $sleeping_time.");
+    sleep( $sleeping_time );
+}
+
+1;
