3494f596df9bed0244437cb1e8f7e8a6228a8afd
yosshi <yosshi@gmail.com>
2009-10-22 13:47:40 +0900

    tiny format fix

--- assets/plugins/CustomFeed-Script/ota_library.pl
+++ assets/plugins/CustomFeed-Script/ota_library.pl
@@ -10,45 +10,45 @@ use Web::Scraper;
 
 my $uri = URI->new('https://www.lib.city.ota.tokyo.jp/idcheck.html');
 
-my $mech = new WWW::Mechanize( autocheck => 1 );
-$mech->agent_alias( 'Windows IE 6' );
+my $mech = new WWW::Mechanize(autocheck => 1);
+$mech->agent_alias('Windows IE 6');
 $mech->get($uri);
 
-my $config = pit_get( "www.lib.city.ota.tokyo.jp", require => {
-  "username" => "username for ota city library",
-  "password" => "password for ota city library"
+my $config = pit_get('www.lib.city.ota.tokyo.jp', require => {
+    "username" => "username for ota city library",
+    "password" => "password for ota city library",
 });
 
 $mech->submit_form(
-  fields => {
-    UID  => $config->{username},
-    PASS => $config->{password},
-  }
+    fields => {
+        UID  => $config->{username},
+        PASS => $config->{password},
+    }
 );
 
 $mech->follow_link(text => "貸出照会へ");
 
 my $s = scraper {
-  process "table.FULL tbody tr",
-    "list[]" => scraper {
-      process "//td[2]", "title" => "TEXT";
-      process "//td[5]", "date" => "TEXT";
-    };
+    process "table.FULL tbody tr",
+      "list[]" => scraper {
+          process "//td[2]", "title" => "TEXT";
+          process "//td[5]", "date"  => "TEXT";
+      };
 };
 
 my $res = $s->scrape($mech->content, $mech->uri);
 
 my $feed = {
-  title => "大田区図書館返却期限"
+    title => "大田区図書館返却期限"
 };
 
 for my $menu (@{$res->{list}}) {
-  if ($menu->{title} and $menu->{date}){
-    push @{$feed->{entries}}, {
-      title => $menu->{title},
-      date  => munge_datetime($menu->{date}),
-    };
-  }
+    if ($menu->{title} and $menu->{date}){
+        push @{$feed->{entries}}, {
+            title => $menu->{title},
+            date  => munge_datetime($menu->{date}),
+        };
+    }
 }
 
 use YAML;
@@ -56,14 +56,14 @@ binmode STDOUT, ":utf8";
 print Dump $feed;
 
 sub munge_datetime {
-  my ($date)	   =  @_;
-  $date		   =~ m!(\d{1,2})月(\d{1,2})日! or die "No match: $date";
-  my($month, $day) =  ($1, $2);
-  my $dt	   =  DateTime->new(
-    year      => DateTime->now->year,
-    month     => $month,
-    day	      => $day,
-    time_zone => 'Asia/Tokyo',
-  );
-  return $dt->ymd;
+    my ($date) =  @_;
+    $date =~ m!(\d{1,2})月(\d{1,2})日! or die "No match: $date";
+    my ($month, $day) = ($1, $2);
+    my $dt = DateTime->new(
+        year      => DateTime->now->year,
+        month     => $month,
+        day       => $day,
+        time_zone => 'Asia/Tokyo',
+    );
+    return $dt->ymd;
 }
