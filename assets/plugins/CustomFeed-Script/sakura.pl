a93aaf54f4097d3c8f15274a4b8ff0195258e596
yosshi <yosshi@gmail.com>
2009-10-22 14:01:48 +0900

    another some tiny fix

--- assets/plugins/CustomFeed-Script/sakura.pl
+++ assets/plugins/CustomFeed-Script/sakura.pl
@@ -8,47 +8,47 @@ use Web::Scraper;
 use URI;
 use YAML;
 
-my $uri = URI->new("http://www.teraken.co.jp/menu/lunchmenu/index.html");
-my $s	= scraper {
-  process "table tr",
-    "list[]" => scraper {
-      process 'div[align="right"]', "date" => 'TEXT';
-      process 'div[align="left"]',  "text" => 'TEXT';
-      result 'date','text';
-    };
-  result 'list';
+my $uri = URI->new('http://www.teraken.co.jp/menu/lunchmenu/index.html');
+my $s = scraper {
+    process "table tr",
+      "list[]" => scraper {
+          process 'div[align="right"]', "date" => 'TEXT';
+          process 'div[align="left"]',  "text" => 'TEXT';
+          result 'date','text';
+      };
+    result 'list';
 };
 
 my $scr = $s->scrape($uri);
 
 my $feed = {
-  title => "さくら水産",
-  link  => $uri->as_string,
+    title => "さくら水産",
+    link  => $uri->as_string,
 };
 
 for my $menu (@{$scr}) {
-  if ($menu->{text} and $menu->{date}){
-    push @{$feed->{entries}}, {
-      title => $menu->{text},
-      date  => munge_datetime($menu->{date}),
-    };
-  }
+    if ($menu->{text} and $menu->{date}){
+        push @{$feed->{entries}}, {
+            title => $menu->{text},
+            date  => munge_datetime($menu->{date}),
+        };
+    }
 }
 
 binmode STDOUT, ":utf8";
 print YAML::Dump $feed;
 
 sub munge_datetime {
-  my ($date)	   =  @_;
-  $date		   =~ m!(\d{1,2})月(\d{1,2})日! or die "No match: $date";
-  my($month, $day) =  ($1, $2);
-  my $dt	   =  DateTime->new(
-    year      => DateTime->now->year,
-    month     => $month,
-    day	      => $day,
-    hour      => '12',
-    minute    => '00',
-    time_zone => 'Asia/Tokyo',
-  );
-  return DateTime::Format::W3CDTF->format_datetime($dt);
+    my ($date) =  @_;
+    $date =~ m!(\d{1,2})月(\d{1,2})日! or die "No match: $date";
+    my ($month, $day) = ($1, $2);
+    my $dt =  DateTime->new(
+        year      => DateTime->now->year,
+        month     => $month,
+        day       => $day,
+        hour      => '12',
+        minute    => '00',
+        time_zone => 'Asia/Tokyo',
+    );
+    return DateTime::Format::W3CDTF->format_datetime($dt);
 }
--- assets/plugins/CustomFeed-Script/yahoo_fortune.pl
+++ assets/plugins/CustomFeed-Script/yahoo_fortune.pl
@@ -13,33 +13,32 @@ my $s = scraper {
     process "title", title => 'TEXT';
     process "#mnc",
       "list[]" => scraper {
-	  process "div.mg10t div.yftn-md20 div.inr-wr div.bg01 div.bg02 p", "subtitle" => "TEXT";
-	  process "div.mg10t",  "report0" => "HTML";
-	  process "#lnk01",     "report1" => "HTML";
-	  process "#lnk02",     "report2" => "HTML";
-	  process "#lnk03",     "report3" => "HTML";
-	  process "#lnk04",     "report4" => "HTML";
-	  process "#menu_love", "report5" => "HTML";
-	  process "div.mg20t",  "report6" => "HTML";
-	  process "div.mg5t"  , "report7" => "HTML";
+          process "div.mg10t div.yftn-md20 div.inr-wr div.bg01 div.bg02 p", "subtitle" => "TEXT";
+          process "div.mg10t",  "report0" => "HTML";
+          process "#lnk01",     "report1" => "HTML";
+          process "#lnk02",     "report2" => "HTML";
+          process "#lnk03",     "report3" => "HTML";
+          process "#lnk04",     "report4" => "HTML";
+          process "#menu_love", "report5" => "HTML";
+          process "div.mg20t",  "report6" => "HTML";
+          process "div.mg5t"  , "report7" => "HTML";
       };
 };
 
 my $res = $s->scrape($uri);
 
 my $feed = {
-  title => $res->{title}
+    title => $res->{title}
 };
 
-for my $entry (@{ $res->{list} }) {
-  push @{$feed->{entries}}, {
-    title => $entry->{subtitle},
-    body  => $entry->{report0} . $entry->{report1} . $entry->{report2} . $entry->{report3} . $entry->{report4} . $entry->{report5} . $entry->{report6} . $entry->{report7},
-    link  => $url,
-  };
+for my $entry (@{$res->{list}}) {
+    push @{$feed->{entries}}, {
+        title => $entry->{subtitle},
+        body  => $entry->{report0} . $entry->{report1} . $entry->{report2} . $entry->{report3} . $entry->{report4} . $entry->{report5} . $entry->{report6} . $entry->{report7},
+        link  => $url,
+    };
 }
 
 use YAML;
 binmode STDOUT, ":utf8";
 print YAML::Dump $feed;
-
