4700fd1cb865e0f636ae9d432b2d9522f0b8498f
yosshi <yosshi@gmail.com>
2009-11-27 13:39:31 +0900

    sqex members party battle 2

---
+++ assets/plugins/CustomFeed-Script/sqex-pb2.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+use strict;
+use utf8;
+use DateTime;
+use DateTime::Format::W3CDTF;
+use URI;
+use WWW::Mechanize;
+use Config::Pit;
+use Web::Scraper;
+
+my $uri = URI->new('http://member.square-enix.com/jp/login/login.php');
+
+my $mech = new WWW::Mechanize(autocheck => 1);
+$mech->agent_alias('Windows IE 6');
+$mech->get($uri);
+
+my $config = pit_get('member.square-enix.com', require => {
+    "username" => "username for sqex members",
+    "password" => "password for sqex members",
+});
+
+$mech->submit_form(
+    fields => {
+        login_id => $config->{username},
+        password => $config->{password},
+    }
+);
+
+$mech->get('http://member.square-enix.com/jp/avatar/partybattle2/party.php');
+
+my $s = scraper {
+    process "#main",
+      "list[]" => scraper {
+          process "div.times",           "title" => "TEXT";
+          process "div.timeinfo p.time", "date"  => "TEXT";
+      };
+};
+
+my $res = $s->scrape($mech->content, $mech->uri);
+
+my $feed = {
+    title => "SQEX PartyBattle2"
+};
+
+for my $menu (@{$res->{list}}) {
+    if ($menu->{title} and $menu->{date}){
+        push @{$feed->{entries}}, {
+            title => $menu->{title},
+            date  => $menu->{date},
+        };
+    }
+}
+
+use YAML;
+binmode STDOUT, ":utf8";
+print Dump $feed;
+
