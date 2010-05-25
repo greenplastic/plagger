d61ba4495c606aaeb618f7d805a712e21395ed45
yosshi <yosshi@gmail.com>
2010-05-25 15:05:41 +0900

    shimomaruko_library_closed_day.pl

---
+++ assets/plugins/CustomFeed-Script/shimomaruko_library_closed_day.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+use strict;
+use utf8;
+use DateTime;
+use DateTime::Format::W3CDTF;
+use URI;
+use Web::Scraper;
+use Lingua::JA::Regular::Unicode;
+
+my $s = scraper {
+    process "table",
+      "list[]" => scraper {
+          process "//caption", "title" => "TEXT";
+          process '//td[@class="CLOSE"]', "date"  => "TEXT";
+      };
+};
+
+my $uri = new URI('http://www.lib.city.ota.tokyo.jp/clis/calender?LIB=013&RTN=15');
+
+my $res = $s->scrape($uri);
+
+my $feed = {
+    title => "下丸子図書館閉館日"
+};
+
+for my $menu (@{$res->{list}}) {
+    if ($menu->{title} and $menu->{date}){
+        push @{$feed->{entries}}, {
+            title => munge_month($menu->{title}) . "月の閉館日",
+            date  => munge_datetime($menu->{title}, $menu->{date}),
+        };
+    }
+}
+
+use YAML;
+binmode STDOUT, ":utf8";
+print Dump $feed;
+
+sub munge_month {
+    my $month = shift;
+    $month =~ m/([１|２|３|４|５|６|７|８|９|１０|１１|１２])月/ or die "No match: $month";
+    return alnum_z2h($1);
+}
+
+sub munge_datetime {
+    my ($month, $day) = @_;
+    $month =~ m/([１|２|３|４|５|６|７|８|９|１０|１１|１２])月/ or die "No match: $month";
+    my $m = $1;
+    $m = alnum_z2h($m);
+    $day = alnum_z2h($day);
+    my $dt = DateTime->new(
+        year      => DateTime->now->year,
+        month     => $m,
+        day       => $day,
+        time_zone => 'Asia/Tokyo',
+    );
+    return $dt->ymd;
+}
